ad5715e26a5fcc418fd8c3af0aa6a48551ec0d8b
kentdani <43226709+kentdani@users.noreply.github.com>
2021-09-20 22:56:57 -0300

    Enumeradores

---
+++ MensagemCliente.cs
@@ -0,0 +1,9 @@
+﻿namespace Enumeradores
+{
+    public static class MensagemCliente
+    {
+
+        public static string ConteinerNaoEncontrado() { return "O contêiner não foi encontrado."; }
+        public static string MovimentacaoNaoEncontrada() { return "A movimentação de carga não foi encontrada."; }
+    }
+}
